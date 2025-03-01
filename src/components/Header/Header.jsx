c6eaa5b7273153398e5b2fe2fb1c0baa1bb791a3
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-03-02 01:03:22 +0530

    Enhance button hover effect in Header component and add padding to PostForm submission form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -50,7 +50,7 @@ function Header(){
                                 <button
                                 onClick={() => navigate(item.slug)}
                                 className="inline-block px-6 py-2 duration-200
-                                 hover:ng-blue-100 rounded-full"
+                               hover:bg-blue-100 rounded-full"
                                 >{item.name}</button>
                             </li>
                           ) : null
